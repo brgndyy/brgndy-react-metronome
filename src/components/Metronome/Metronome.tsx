25cc2bc61715a1ad61feb37e7798fef9fb4b2801
brgndy <brgndy96@gmail.com>
2023-12-20 09:56:29 +0900

    Feat: create Metronome Component

--- src/components/Metronome/Metronome.tsx
+++ src/components/Metronome/Metronome.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect, PropsWithChildren } from "react";
 import tickSound from "./tick.wav";
 import tockSound from "./tock.wav";
 
-export default function Metronome() {
-  return <div>Metronome</div>;
+interface MetronomeProps extends PropsWithChildren<{}> {
+  type?: "number" | "range";
+  className: string;
+  minBpm: number;
+  maxBpm: number;
+}
+
+export default function Metronome(props: MetronomeProps) {
+  const { type = "number", className = "", minBpm = 1, maxBpm = 300 } = props;
+  const [tick, setTick] = useState<HTMLAudioElement>();
+  const [tock, setTock] = useState<HTMLAudioElement>();
+  const [bpm, setBpm] = useState<number>(60);
+
+  useEffect(() => {
+    setTick(new Audio(tickSound));
+    setTock(new Audio(tockSound));
+  }, []);
+
+  return (
+    <input
+      type={type}
+      className={className}
+      min={minBpm}
+      max={maxBpm}
+      value={bpm}
+    />
+  );
 }
