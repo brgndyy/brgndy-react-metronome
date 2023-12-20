50db2f53bbae6387c3eb1502fc899e4b2542c300
brgndy <brgndy96@gmail.com>
2023-12-20 15:09:39 +0900

    Feat: add constants

--- src/constants/condition.ts
+++ src/constants/condition.ts
@@ -3,6 +3,8 @@ import deepFreeze from "../utils/deepFreeze";
 const CONDITION = deepFreeze({
   min_bpm: 1,
   max_bpm: 300,
+  min_metronome_count: 1,
+  max_metronome_count: 4,
 });
 
 export default CONDITION;
