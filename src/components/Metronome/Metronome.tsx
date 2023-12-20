f167615a231c1cb536b2eb57431107a38c86a8df
brgndy <brgndy96@gmail.com>
2023-12-20 15:10:04 +0900

    Feat: add codes of Metronome

--- src/components/Metronome/Metronome.tsx
+++ src/components/Metronome/Metronome.tsx
@@ -1,9 +1,16 @@
-import React, { useState, useEffect, PropsWithChildren } from "react";
+import React from "react";
+import {
+  useState,
+  useEffect,
+  PropsWithChildren,
+  useCallback,
+  useRef,
+} from "react";
 import tickSound from "./tick.wav";
 import tockSound from "./tock.wav";
 import CONDITION from "../../constants/condition";
 import { useMetronome } from "../MetronomeContext/MetronomeContext";
-import { withMetronomeContext } from "../MetronomeContext/WithMetronomeContext";
+import { withMetronomeContext } from "../MetronomeContext/withMetronomeContext";
 
 interface MetronomeProps extends PropsWithChildren<{}> {
   type: "number" | "range";
@@ -13,7 +20,8 @@ interface MetronomeProps extends PropsWithChildren<{}> {
 }
 
 function Metronome(props: MetronomeProps) {
-  const { isPlaying } = useMetronome();
+  const bpmRef = useRef<HTMLInputElement | null>(null);
+  // const { isPlaying } = useMetronome();
   const { type = "number", className = "" } = props;
   const [minBpm, setMinBpm] = useState<number>(
     props.minBpm || CONDITION.min_bpm
@@ -24,6 +32,25 @@ function Metronome(props: MetronomeProps) {
   const [tick, setTick] = useState<HTMLAudioElement>();
   const [tock, setTock] = useState<HTMLAudioElement>();
   const [bpm, setBpm] = useState<number>(60);
+  const [count, setCount] = useState<number>(1);
+  const [first, setFirst] = useState(false);
+  const [blur, setBlur] = useState(false);
+
+  const metronomePlayHandler = useCallback(() => {
+    if (!first && tick) {
+      setFirst(true);
+      tick.play();
+    } else if (count === CONDITION.min_metronome_count && tick) {
+      tick.play();
+    } else if (count < CONDITION.max_metronome_count + 1 && tock) {
+      tock.play();
+    }
+    if (count >= CONDITION.max_metronome_count) {
+      setCount(CONDITION.min_metronome_count);
+    } else {
+      setCount((prevCount) => prevCount + 1);
+    }
+  }, [count, tick, tock, first]);
 
   useEffect(() => {
     setTick(new Audio(tickSound));
@@ -33,15 +60,84 @@ function Metronome(props: MetronomeProps) {
     setMaxBpm((prev) => Math.min(CONDITION.max_bpm, props.maxBpm || prev));
   }, [props.minBpm, props.maxBpm]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      metronomePlayHandler();
+    }, (60 / bpm) * 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [bpm, metronomePlayHandler]);
+
+  const bpmChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let newBpm = parseInt(e.target.value, 10);
+    if (newBpm < CONDITION.min_bpm) {
+      newBpm = CONDITION.min_bpm;
+    }
+    if (newBpm > CONDITION.max_bpm) {
+      newBpm = CONDITION.max_bpm;
+      if (bpmRef.current) {
+        bpmRef.current.blur();
+      }
+    }
+    setBpm(newBpm);
+    if (bpmRef.current && Number.isNaN(newBpm)) {
+      bpmRef.current.focus();
+    }
+    if (bpmRef.current && newBpm > CONDITION.max_bpm) {
+      bpmRef.current.blur();
+    }
+  };
+
+  const bpmBlurHandler = () => {
+    if (bpmRef.current && bpm > CONDITION.max_bpm) {
+      setBpm(CONDITION.max_bpm);
+      bpmRef.current.blur();
+    }
+    if (bpmRef.current && Number.isNaN(bpm)) {
+      setBpm(CONDITION.min_bpm);
+      bpmRef.current.blur();
+    }
+    setBlur(true);
+  };
+
+  const keyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (bpmRef.current && e.key === "Enter") {
+      const newBpm = parseInt(e.currentTarget.value, 10);
+
+      if (Number.isNaN(newBpm)) {
+        setBpm(CONDITION.min_bpm);
+      }
+      e.currentTarget.blur();
+    }
+  };
+
+  const focusHandler = () => {
+    setBlur(false);
+  };
+
+  useEffect(() => {
+    if (bpmRef.current && bpmRef.current !== document.activeElement) {
+      bpmRef.current.blur();
+    }
+  }, [bpmRef]);
+
   return (
     <input
+      // ref={bpmRef}
       type={type}
       className={className}
+      step={1}
       min={minBpm}
       max={maxBpm}
       value={bpm}
+      onBlur={bpmBlurHandler}
+      onFocus={focusHandler}
+      onChange={bpmChangeHandler}
+      onKeyDown={keyDownHandler}
     />
   );
 }
 
-export default withMetronomeContext(Metronome);
+export default Metronome;
