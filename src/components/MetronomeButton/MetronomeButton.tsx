e6ae8b0b4ed1c7ef5bce3f25125cd89af157e4ee
brgndy <brgndy96@gmail.com>
2023-12-20 15:10:21 +0900

    Refactor:  refresh for debugging

--- src/components/MetronomeButton/MetronomeButton.tsx
+++ src/components/MetronomeButton/MetronomeButton.tsx
@@ -1,6 +1,6 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useState, useEffect } from "react";
 import { useMetronome } from "../MetronomeContext/MetronomeContext";
-import { withMetronomeContext } from "../MetronomeContext/WithMetronomeContext";
+import { withMetronomeContext } from "../MetronomeContext/withMetronomeContext";
 
 interface MetronomeButtonProps extends PropsWithChildren<{}> {
   className: string;
@@ -9,17 +9,20 @@ interface MetronomeButtonProps extends PropsWithChildren<{}> {
 
 function MetronomeButton(props: MetronomeButtonProps) {
   const { className = "", buttonTexts } = props;
-  const { isPlaying, setIsPlaying } = useMetronome();
+  const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
-    setIsPlaying(!isPlaying);
+    setClicked(!clicked);
   };
 
   return (
-    <button onClick={handleClick} className={className}>
-      {isPlaying ? buttonTexts[1] : buttonTexts[0]}
-    </button>
+    <>
+      <button className={className} onClick={handleClick}>
+        {(buttonTexts[1], buttonTexts[0])}
+      </button>
+      {clicked ? "클릭" : "클릭취소"}
+    </>
   );
 }
 
-export default withMetronomeContext(MetronomeButton);
+export default MetronomeButton;
