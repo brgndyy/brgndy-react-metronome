93c47b9e1558ccd80d6f02b05635ba4db401d910
brgndy <brgndy96@gmail.com>
2023-12-26 07:54:59 +0900

    Fix: fix rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import url from "@rollup/plugin-url";
 import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -24,12 +25,14 @@ export default [
       commonjs(),
 
       terser(),
+      peerDepsExternal(),
       url({
         fileName: "[name][extname]",
         include: ["**/*.wav"],
         limit: 100000,
       }),
     ],
+    external: ["react", "react-dom"],
   },
   {
     input: "dist/cjs/types/src/index.d.ts",
